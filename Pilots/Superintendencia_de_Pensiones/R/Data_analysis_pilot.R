0efb36f8d6d1d4f65a58b648e13daf26caf1e010
Denise Laroze <deniselaroze@users.noreply.github.com>
2022-06-11 06:41:37 -0400

    Update Data_analysis_pilot.R

--- Pilots/Superintendencia_de_Pensiones/R/Data_analysis_pilot.R
+++ Pilots/Superintendencia_de_Pensiones/R/Data_analysis_pilot.R
@@ -11,6 +11,8 @@ library(readr)
 library(matrixStats)
 library(BBmisc)
 library(dplyr)
+library(ggpubr)
+library(ggplot2)
 
 setwd("C:/Users/Denise Laroze/Dropbox/Sitios web/Datos Piloto/Superintendencia de Pensiones/online")
 
@@ -20,10 +22,13 @@ load("Pilot_data.Rdata")
 
 df<-encuestas
 
+
+
+
 # Data Manipulation
 
 web<-as.factor(df$website)
-df$treat<-recode(web, 
+df$Treatments<-recode(web, 
                  "1" = "Profile",
                  "2" = "Video",
                  "3" = "VideoProfile",
@@ -34,6 +39,9 @@ df$treat<-recode(web,
                  "8" = "Baseline"
 )
 
+
+df$utilidad<-as.numeric(df$InfoUtil_1)
+df$toomuchinfo<-as.numeric(df$InfoAbruma2_1)
 
 ######
 
@@ -46,69 +54,136 @@ names(df)
 
 
 
-###########
-ggline(pilot_data, x = "Treat", y = "correct_response", add = "mean_se",
-       color = "Gender", palette = "jco") +
+
+
+#### Graphics parameters
+pilot_data<-df
+my_comparisons <- rev(list( c("Baseline", "Profile"), c("Profile", "Video"), c("Profile", "VideoProfile"), c("Video", "VideoProfile"), c("Video", "Baseline"), c("Baseline", "VideoProfile") ))
+
+
+
+### Color theme
+theme_gppr <- function(){ 
+  font <- "Georgia"   #assign font family up front
+  
+  ggthemes::theme_economist_white() %+replace%    #replace elements we want to change
+    
+    theme(
+      legend.position = "bottom",
+      #grid elements
+      panel.grid.major = element_blank(),    #strip major gridlines
+      panel.grid.minor = element_blank(),    #strip minor gridlines
+      
+      #since theme_minimal() already strips axis lines, 
+      #we don't need to do that again
+      
+      #text elements
+      plot.title = element_text(             #title
+        family = font,            #set font family
+        size = 20,                #set font size
+        face = 'bold',            #bold typeface
+        hjust = 0,                #left align
+        vjust = 2),               #raise slightly
+      
+      plot.subtitle = element_text(          #subtitle
+        family = font,            #font family
+        size = 14),               #font size
+      
+      plot.caption = element_text(           #caption
+        family = font,            #font family
+        size = 9,                 #font size
+        hjust = 1),               #right align
+      
+      axis.title = element_text(             #axis titles
+        family = font,            #font family
+        size = 10),               #font size
+      
+      axis.text = element_text(              #axis text
+        family = font,            #axis famuly
+        size = 10),                #font size
+      
+      axis.text.x = element_text(            #margin for axis text
+        margin=margin(5, b = 10)),
+      axis.title.x = element_text(size=12),
+      axis.title.y = element_text(size=12, angle = 90, vjust = +3)
+      
+      #since the legend often requires manual tweaking 
+      #based on plot content, don't define it here
+    )
+}
+
+
+
+### 
+
+h <- round(mean(pilot_data$correct_response, na.rm = TRUE), digits = 1)
+
+
+  ggplot(pilot_data, aes(y = correct_response, x = Treatments, color=Treatments)) +
+  geom_boxplot() +
+  geom_jitter(width=0.15) +
   xlab("Treatments") +
-  ylab("Number of Clicks") +
-  ggpubr::stat_compare_means(aes(group=Gender), label = "p.signif", method = "wilcox.test") +
-  scale_y_continuous(limits = c(0,50)) 
-  theme_gppr()
+  ylab("Corrects Responses") +
+  geom_hline(aes(yintercept = h), linetype = 2, color = "gray") +
+  geom_text(aes(y=h+0.5, label=paste0("Mean ", prettyNum(h,big.mark=",")), x=0.1), colour='gray', hjust=-0.1 , vjust = 1) +
+  #geom_text(aes(h, label=h, hjust=-0.1)) +
+  # geom_text(aes(y=mean(pilot_data$n_clicks),
+  #               label=prettyNum(round(mean(pilot_data$n_clicks)),big.mark=","), x=1),
+  #           colour='blue' )   +
+  theme_gppr()  +
+  ggsci::scale_color_aaas() + 
+  ggpubr::stat_compare_means(method = "anova", vjust = 1, hjust = -0.05) +      # Add global p-value
+  ggpubr::stat_compare_means(comparisons = my_comparisons, 
+                             label = "p.signif", method = "wilcox.test")
+#ggpubr::stat_compare_means(method = "wilcox.test")
+#scale_fill_distiller()
+#paletteer::scale_color_paletteer_d("colorBlindness::paletteMartin")
+#scale_colour_brewer(type = "seq", palette = "Spectral")
+
+
+
+table(df$correct_response, df$Treatments)
 
 
 
 
+### Utilidad
+ggplot(pilot_data, aes(y = utilidad, x = Treatments, color=Treatments)) +
+  geom_boxplot() +
+  geom_jitter(width=0.15) +
+  xlab("Treatments") +
+  ylab("Usefull information") +
+  geom_hline(aes(yintercept = h), linetype = 2, color = "gray") +
+  geom_text(aes(y=h+0.5, label=paste0("Mean ", prettyNum(h,big.mark=",")), x=0.1), colour='gray', hjust=-0.1 , vjust = 1) +
+  #geom_text(aes(h, label=h, hjust=-0.1)) +
+  # geom_text(aes(y=mean(pilot_data$n_clicks),
+  #               label=prettyNum(round(mean(pilot_data$n_clicks)),big.mark=","), x=1),
+  #           colour='blue' )   +
+  theme_gppr()  +
+  ggsci::scale_color_aaas() + 
+  ggpubr::stat_compare_means(method = "anova", vjust = 1, hjust = -0.05) +      # Add global p-value
+  ggpubr::stat_compare_means(comparisons = my_comparisons, 
+                             label = "p.signif", method = "wilcox.test")
 
 
 
+### too much info
+ggplot(pilot_data, aes(y = toomuchinfo , x = Treatments, color=Treatments)) +
+  geom_boxplot() +
+  geom_jitter(width=0.15) +
+  xlab("Treatments") +
+  ylab("Too much information") +
+  geom_hline(aes(yintercept = h), linetype = 2, color = "gray") +
+  geom_text(aes(y=h+0.5, label=paste0("Mean ", prettyNum(h,big.mark=",")), x=0.1), colour='gray', hjust=-0.1 , vjust = 1) +
+  #geom_text(aes(h, label=h, hjust=-0.1)) +
+  # geom_text(aes(y=mean(pilot_data$n_clicks),
+  #               label=prettyNum(round(mean(pilot_data$n_clicks)),big.mark=","), x=1),
+  #           colour='blue' )   +
+  theme_gppr()  +
+  ggsci::scale_color_aaas() + 
+  ggpubr::stat_compare_means(method = "anova", vjust = 1, hjust = -0.05) +      # Add global p-value
+  ggpubr::stat_compare_means(comparisons = my_comparisons, 
+                             label = "p.signif", method = "wilcox.test")
 
 
-
-
-
-
-
-#### OLD ##############
-## Respuestas correctas
-df %>%
-  group_by(treat) %>%
-  summarise_at(vars(correct_response), list(mean = mean, sd= sd), na.rm=T)
-
-
-## info abrumadora por tratamiento
-df %>%
-  group_by(treat) %>%
-  summarise_at(vars(InfoAbruma2_1), list(mean = mean, sd= sd), na.rm=T)
-
-## info Util
-df %>%
-  group_by(treat) %>%
-  summarise_at(vars(InfoUtil_1), list(mean = mean, sd= sd), na.rm=T)
-
-
-## Curiosity
-df %>%
-  group_by(treat) %>%
-  summarise_at(vars(Curiosity_1), list(mean = mean, sd= sd), na.rm=T)
-
-
-
-
-##Confidence
-df %>%
-  group_by(treat) %>%
-  summarise_at(vars(Confidence_1), list(mean = mean, sd= sd), na.rm=T)
-
-
-
-
-
-
-
-
-
-
-
-
-names(dv)
 
