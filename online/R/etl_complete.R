b8559355c78bf5dfd57635fb9d06cfc159a88755
Denise Laroze <deniselaroze@users.noreply.github.com>
2022-08-03 17:48:37 -0400

    data Analysys Denise

--- online/R/etl_complete.R
+++ online/R/etl_complete.R
@@ -1,39 +1,38 @@
-path_datos <- "C:/Users/Usuario/Documents/INVESTIGACION/MiInvestigacion/Pensions-Website-Design/online/data/"
-
-
 pilot_data <- sitios_complete %>%
   right_join(encuestas, by = "useridn") %>%
   filter(useridn !=0) %>%
   mutate(Age = 2022 - as.numeric(Birth),
          Treatments = factor(case_when(
-           website.x == "1" ~ "Perfil",
-           website.x == "2" ~ "Video",
-           website.x ==  "3" ~ "VideoPerfil",
-           website.x == "4" ~ "Perfil",
-           website.x == "5" ~ "Video",
-           website.x == "6" ~ "VideoPerfil",
-           website.x == "7" ~ "Baseline",
-           website.x == "8" ~ "Baseline"), levels = c("Baseline", "Perfil", "Video", "VideoPerfil")),
+           website == "1" ~ "Perfil",
+           website == "2" ~ "Video",
+           website ==  "3" ~ "VideoPerfil",
+           website == "4" ~ "Perfil",
+           website == "5" ~ "Video",
+           website == "6" ~ "VideoPerfil",
+           website == "7" ~ "Baseline",
+           website == "8" ~ "Baseline"), levels = c("Baseline", "Perfil", "Video", "VideoPerfil")),
          OptOut = case_when(
            terminaron == "si" & contesta != "C" ~ "In",
            TRUE ~ "Out"
          ),
          Pension_Type = factor(case_when(
-           website.x == "1" ~ "Public",
-           website.x == "2" ~ "Public",
-           website.x == "3" ~ "Public",
-           website.x == "8" ~ "Public",
-           website.x == "4" ~ "Private",
-           website.x == "5" ~ "Private",
-           website.x == "6" ~ "Private",
-           website.x == "7" ~ "Private"), levels = c("Public", "Private"))) %>% 
+           website == "1" ~ "Public",
+           website == "2" ~ "Public",
+           website == "3" ~ "Public",
+           website == "8" ~ "Public",
+           website == "4" ~ "Private",
+           website == "5" ~ "Private",
+           website == "6" ~ "Private",
+           website == "7" ~ "Private"), levels = c("Public", "Private")),
+         online = ifelse(fecha.x.x == "2022-06-08", 1, 0)) %>% 
   arrange(useridn)
 
-# breaks <- hour(hm("00:00", "6:00", "12:00", "18:00", "23:59"))
-# # labels for the breaks
-# labels <- c("Night", "Morning", "Afternoon", "Evening")
-# 
-# pilot_data$Time_of_day <- cut(x=hour(pilot_data$StartDate), breaks = breaks, labels = labels, include.lowest=TRUE)
+breaks <- hour(hm("00:00", "6:00", "12:00", "18:00", "23:59"))
+# labels for the breaks
+labels <- c("Night", "Morning", "Afternoon", "Evening")
+
+pilot_data$Time_of_day <- cut(x=hour(pilot_data$StartDate), breaks = breaks, labels = labels, include.lowest=TRUE)
+
 
 
 #Overconfidence
@@ -46,9 +45,10 @@ pilot_data$Change_Advisor<-ifelse(pilot_data$PAdvice=="No" & pilot_data$Advisor=
                                   ifelse (pilot_data$PAdvice=="No" & pilot_data$Advisor=="Sí", "> advisor",
                                           ifelse(pilot_data$PAdvice=="Sí" & pilot_data$Advisor=="Sí", "Maintain Yes",
                                                  ifelse(pilot_data$PAdvice=="Sí" & pilot_data$Advisor=="No", "< advisor",
-                                                        ifelse(pilot_data$PAdvice=="No lo ha pensado" & pilot_data$Advisor=="Sí", "> advisor", 
-                                                               ifelse(pilot_data$PAdvice=="No lo ha pensado" & pilot_data$Advisor=="No", "< advisor", "Error"
-                                                               ))))))
+                                                        ifelse (pilot_data$PAdvice=="Sí" & pilot_data$Advisor=="Sí", "Maintain",
+                                                                ifelse(pilot_data$PAdvice=="No lo ha pensado" & pilot_data$Advisor=="Sí", "> advisor", 
+                                                                       ifelse(pilot_data$PAdvice=="No lo ha pensado" & pilot_data$Advisor=="No", "< advisor", "Error"
+                                                                       )))))))
 
 
 
@@ -99,5 +99,7 @@ pilot_data$present_bias<-tmp$pb
 rm(df1, df2, df, tmp)
 
 
-
 saveRDS(pilot_data, paste0(path_datos, "pilot_data.rds"))
+
+save(pilot_data, file=paste0(path_datos, "pilot_data.Rdata"))
+
